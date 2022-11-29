d721d29788df4735a7bc985440370f7756c93a51
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2022-11-29 14:03:46 +0000

    add checkbox input component

---
+++ packages/frontend/src/app/shared/components/form/checkbox-input/checkbox-input.component.ts
@@ -0,0 +1,67 @@
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { EventOption } from "@core";
+
+@Component({
+  selector: 'app-checkbox-input',
+  templateUrl: './checkbox-input.component.html',
+  styleUrls: ['./checkbox-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxInputComponent),
+      multi: true,
+    },
+  ],
+})
+export class CheckboxInputComponent implements OnInit {
+  @Input() required: boolean = false;
+  @Input() leftLabel?: string;
+  @Input() rightLabel?: string;
+  @Input() identifier!: string;
+  @Input() disabled: boolean = false;
+  @Input('value') _value?: string | number = '';
+  @Input() helperText?: string;
+  @Input() error: boolean = false;
+  @Output() emitInputChange = new EventEmitter<EventOption>();
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  writeValue(value: string): void {
+    if (value) this._value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onValueChange(value: EventOption['value']) {
+    this.emitInputChange.emit({ value, id: this.identifier });
+  }
+}
