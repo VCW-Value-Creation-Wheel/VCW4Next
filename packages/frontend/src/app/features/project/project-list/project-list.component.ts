706b42e4540eac8ac32958c84e425530159222b7
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2022-12-19 15:34:30 +0000

    use project card component in project list

--- packages/frontend/src/app/features/project/project-list/project-list.component.ts
+++ packages/frontend/src/app/features/project/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '@core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -10,7 +11,13 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export class ProjectListComponent implements OnInit {
 
   faPlus = faPlus;
-  cards = [0,1,2,3,4,5,6]
+  cards: Project[] = [
+    {title: 'Project 1', description: 'Description 1'},
+    {title: 'Project 2', description: 'Description 2'},
+    {title: 'Project 3', description: 'Description 3'},
+    {title: 'Project 4', description: 'Description 4'},
+    {title: 'Project 5', description: 'Description 5'}
+  ];
 
   constructor() { }
 
