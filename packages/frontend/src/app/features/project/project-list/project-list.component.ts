0fb7dbd79fd8c5c8efcf3703fb8488e1c8bc8323
Flávio Rosa <flavio.rosa@deimos.com.pt>
2022-12-20 15:09:38 +0000

    merge conflits with develop solved

--- packages/frontend/src/app/features/project/project-list/project-list.component.ts
+++ packages/frontend/src/app/features/project/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Project, MockProjectService } from '@core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,11 +10,14 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
-
   faPlus = faPlus;
   projects: Project[] = [];
 
-  constructor(private projectMock: MockProjectService) { }
+  constructor(
+    private projectMock: MockProjectService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     // this loads a mock for testing. Disable this when loading from the back-end.
@@ -25,6 +29,6 @@ export class ProjectListComponent implements OnInit {
   }
 
   addNewProject() {
-    console.log('New Project');
+    this.router.navigate(["/new-project"],{relativeTo: this.route});
   }
 }
