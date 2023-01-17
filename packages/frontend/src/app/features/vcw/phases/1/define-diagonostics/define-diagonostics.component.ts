9780d0544d1a5643e6666afd9709e10aa6682dfd
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2023-01-17 12:18:55 +0000

    apply formcontrols swot analysis forms

--- packages/frontend/src/app/features/vcw/phases/1/define-diagonostics/define-diagonostics.component.ts
+++ packages/frontend/src/app/features/vcw/phases/1/define-diagonostics/define-diagonostics.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { SwotField } from '@core';
+import { FormArray, FormBuilder, FormGroup, UntypedFormGroup } from '@angular/forms';
+import { swotAnalysisConfig, SwotField, swotFieldsConfig } from '@core';
 import { faPlus, faMinus, faTimes, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,6 +9,8 @@ import { faPlus, faMinus, faTimes, faFloppyDisk } from '@fortawesome/free-solid-
   styleUrls: ['./define-diagonostics.component.scss']
 })
 export class DefineDiagonosticsComponent implements OnInit {
+
+  dataForm: UntypedFormGroup;
 
   faPlus = faPlus;
   faMinus = faMinus;
@@ -25,7 +28,10 @@ export class DefineDiagonosticsComponent implements OnInit {
 
   swotFields: SwotField[] = [];
 
-  constructor() {}
+  constructor(private formbuilder: FormBuilder) {
+    this.dataForm = this.formbuilder.group(swotAnalysisConfig);
+    this.dataForm.controls.swotFields = this.formbuilder.array([]);
+  }
 
 
   ngOnInit(): void {
@@ -56,18 +62,26 @@ export class DefineDiagonosticsComponent implements OnInit {
   }
 
   addField(tabId: number) {
+    (this.dataForm.controls.swotFields as FormArray).push(
+      this.formbuilder.group(swotFieldsConfig)
+    );
     this.swotFields.push({
       id: this.swotFields.length + 1,
       categoryId: tabId,
       title: '',
       description: ''
     });
+    ((this.dataForm.controls.swotFields as FormArray).controls[this.swotFields.length - 1] as FormGroup)
+      .controls.categoryId.setValue(tabId);
   }
 
   removeField(fieldId: number) {
     const fieldToRemove = this.swotFields.find((field) => field.id === fieldId);
     if (fieldToRemove) {
+      const fieldIndex = this.swotFields.indexOf(fieldToRemove);
       this.swotFields.splice(this.swotFields.indexOf(fieldToRemove), 1);
+
+      (this.dataForm.controls.swotFields as FormArray).removeAt(fieldIndex);
     }
   }
 
@@ -76,6 +90,6 @@ export class DefineDiagonosticsComponent implements OnInit {
   }
 
   onSave() {
-
+    console.log(this.dataForm)
   }
 }
