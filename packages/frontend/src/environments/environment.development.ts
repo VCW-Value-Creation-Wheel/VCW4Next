d2f1f8df553bec747e82bff108538dbdab5890f6
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2023-02-02 11:39:07 +0000

    create development environment file

---
+++ packages/frontend/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    staging: false,
+    identity: {
+      url: 'https://vcw-identity.develop.s4eo.com',
+      realm: 'vcw',
+      clientId: 'vcw-web'
+    }
+  };
