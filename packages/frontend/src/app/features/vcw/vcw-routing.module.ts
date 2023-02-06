fc9f532c0a5929c5294eadfc9738f60e21eae782
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2023-02-06 17:35:47 +0000

    add routing and phase nav configuration

--- packages/frontend/src/app/features/vcw/vcw-routing.module.ts
+++ packages/frontend/src/app/features/vcw/vcw-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NewVcwComponent } from './new-vcw/new-vcw.component';
 import { ChallengeComponent } from './phases/1/challenge/challenge.component';
 import { DefineDiagonosticsComponent } from './phases/1/define-diagonostics/define-diagonostics.component';
+import { CreateIdeasComponent } from './phases/2/create-ideas/create-ideas.component';
 import { VcwPageComponent } from './vcw-page/vcw-page.component';
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
     {
         path: ':vcw_id/phases/1b',
         component: ChallengeComponent
+    },
+    {
+        path: ':vcw_id/phases/2a',
+        component: CreateIdeasComponent
     }
 ];
 
