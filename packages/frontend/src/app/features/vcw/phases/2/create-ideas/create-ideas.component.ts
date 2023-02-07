6b61e0c09df0d4b9cef9e35cbb695dc19bf56fe4
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2023-02-07 18:03:17 +0000

    add dialog to ideas page

--- packages/frontend/src/app/features/vcw/phases/2/create-ideas/create-ideas.component.ts
+++ packages/frontend/src/app/features/vcw/phases/2/create-ideas/create-ideas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PhaseNavigationService } from '@core';
+import { createIdeasConfig, PhaseNavigationService } from '@core';
 import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,10 +12,15 @@ import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 export class CreateIdeasComponent implements OnInit {
 
   faFloppyDisk = faFloppyDisk;
+  
+  dataform: UntypedFormGroup;
 
   constructor(private phaseNavService: PhaseNavigationService,
               private router: Router,
-              private activatedRoute: ActivatedRoute) {}
+              private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder) {
+                this.dataform = formBuilder.group(createIdeasConfig);
+              }
 
   ngOnInit(): void {
     this.phaseNavService.nextPhase$.subscribe((nextPhase) => {
@@ -25,4 +31,12 @@ export class CreateIdeasComponent implements OnInit {
   onSave() {
 
   }
+
+  /* TO DO LIST
+    1 - create form: Formarray, where each entry is an idea
+    2 - idea form attributes: name, entryTypeId, sourceName, sourceDescription (DONE)
+    3 - create dialog for new idea, 2 tabs for manual & from file (IN PROGRESS)
+      3.1 - IMPLMENT VALUE ACCESSOR FOR FILE INPUT
+    4 - display all ideas as a list, with options to edit and delete.
+  */
 }
