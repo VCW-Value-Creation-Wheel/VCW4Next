804808a9569323d60279856d984a28d9a568a171
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2023-02-08 10:56:05 +0000

    add accepted file types to input component

--- packages/frontend/src/app/shared/components/form/input-file/input-file.component.ts
+++ packages/frontend/src/app/shared/components/form/input-file/input-file.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Accept {
+  [key: string]: string;
+}
+
 @Component({
   selector: 'app-input-file',
   templateUrl: './input-file.component.html',
@@ -7,6 +11,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class InputFileComponent implements OnInit {
   @Input() label = 'Input label';
+  @Input() accepts: Accept[] = [{ '*': '*' }];
 
   constructor() {}
 
