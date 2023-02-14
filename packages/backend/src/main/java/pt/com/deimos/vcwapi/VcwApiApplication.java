8c557810ccebca4b1ac81cbcfbea0ecfed3692d2
Patrick Silva <patrick.silva@deimos.com.pt>
2023-02-14 11:17:59 +0000

    fix(backend): configure reflection for the class that is to be deserialized

--- packages/backend/src/main/java/pt/com/deimos/vcwapi/VcwApiApplication.java
+++ packages/backend/src/main/java/pt/com/deimos/vcwapi/VcwApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@RegisterReflectionForBinding
 public class VcwApiApplication {
 
 	public static void main(String[] args) {
