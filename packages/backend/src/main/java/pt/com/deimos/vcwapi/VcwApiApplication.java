08fda141cecac7c4166abd43267374962ba0fae2
Patrick Silva <patrick.silva@deimos.com.pt>
2023-02-14 11:25:39 +0000

    fix(backend): Add annotation import

--- packages/backend/src/main/java/pt/com/deimos/vcwapi/VcwApiApplication.java
+++ packages/backend/src/main/java/pt/com/deimos/vcwapi/VcwApiApplication.java
@@ -1,5 +1,6 @@
 package pt.com.deimos.vcwapi;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
