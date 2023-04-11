16e3fb09411cc46e6f244d89e60770899547beda
flrr <flavio.rosa@deimos.com.pt>
2023-04-11 11:10:20 +0100

    feat: title fixes

--- packages/frontend/src/app/features/vcw/vcw-routing.module.ts
+++ packages/frontend/src/app/features/vcw/vcw-routing.module.ts
@@ -9,6 +9,7 @@ import { CreateIdeasComponent } from './phases/2/create-ideas/create-ideas.compo
 import { PurificationPageComponent } from './phases/2/purification-page/purification-page.component';
 import { VcwPageComponent } from './vcw-page/vcw-page.component';
 import { CriteriaPageComponent } from './phases/2/criteria-page/criteria-page.component';
+import { SelectCriteriaComponent } from './phases/3/select-criteria/select-criteria.component';
 
 const routes: Routes = [
     {
@@ -41,7 +42,7 @@ const routes: Routes = [
     },
     {
         path: ':vcw_id/phases/3b',
-        component: SelectIdeasComponent
+        component: SelectCriteriaComponent
     }
 ];
 
