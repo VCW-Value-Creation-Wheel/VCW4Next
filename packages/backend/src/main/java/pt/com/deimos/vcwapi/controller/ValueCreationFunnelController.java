ef73fc551eaadeea71e046b15592a6ea1aaf3572
Vasco Ribeiro Conde <vasco.conde@deimos.com.pt>
2024-06-03 14:28:59 +0100

    refactor(backend): check if project belongs to user

--- packages/backend/src/main/java/pt/com/deimos/vcwapi/controller/ValueCreationFunnelController.java
+++ packages/backend/src/main/java/pt/com/deimos/vcwapi/controller/ValueCreationFunnelController.java
@@ -1,7 +1,10 @@
 package pt.com.deimos.vcwapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,29 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import pt.com.deimos.vcwapi.dto.valueCreationFunnel.ValueCreationFunnelDTO;
+import pt.com.deimos.vcwapi.entity.ProjectEntity;
+import pt.com.deimos.vcwapi.exceptions.NotFoundException;
 import pt.com.deimos.vcwapi.service.ValueCreationFunnelService;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/test/v1/projects/{project_id}/vcws/{vcw_id}/ValueCreationFunnel")
+@RequestMapping("/v1/projects/{project_id}/vcws/{vcw_id}/ValueCreationFunnel")
 @Tag(name = "Value Creation Funnel", description = "Value Creation Funnel endpoints")
 public class ValueCreationFunnelController {
 
     @Autowired
     ValueCreationFunnelService valueCreationFunnelService;
 
-
-    //@GetMapping("/valueCreationFunnelDTO")
-    //ValueCreationFunnelDTO getValueCreationFunnelDTO () {
-    //    
-    //    ValueCreationFunnelDTO vcfObj = valueCreationFunnelService.generateVCWValueCreationFunnelDTOObject (14L, true);
-    //
-    //    return vcfObj;
-    //}
-
     @GetMapping("")
+    @Operation(summary = "Run Value Creation Funnel (VCF) and returns result object for a given VCW.")
     ResponseEntity<ValueCreationFunnelDTO> getOValueCreationFunnelDTO(
+            @AuthenticationPrincipal Jwt principal,
             @PathVariable(value = "project_id") Long projectId,
             @PathVariable(value = "vcw_id") Long vcwId) {
+
+        Optional<ProjectEntity> project =
+                valueCreationFunnelService.findProjectByIdAndUser(projectId, principal.getSubject());
+        if (project.isEmpty())
+            throw new NotFoundException("Project not found.");
 
         ValueCreationFunnelDTO vcfObj = valueCreationFunnelService.generateVCWValueCreationFunnelDTOObject(vcwId, true);
 
