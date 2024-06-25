75b917d097c5239314118c819c7baaf11f67a540
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2024-06-25 17:13:48 +0100

    feat: save context information for project and vcw

--- packages/frontend/src/app/features/main-layout/main-layout.component.ts
+++ packages/frontend/src/app/features/main-layout/main-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { PhaseNavigationService } from '@core';
+import { ContextService, PhaseNavigationService, ProjectsService, VcwService } from '@core';
 import { KeycloakService } from 'keycloak-angular';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-layout',
@@ -21,12 +21,18 @@ export class MainLayoutComponent implements OnInit, OnDestroy {
   constructor(private route: Router,
               private activatedRoute: ActivatedRoute,
               private phaseNavigationService: PhaseNavigationService,
-              private keycloak: KeycloakService) { }
+              private keycloak: KeycloakService,
+              private contextService: ContextService,
+              private projectService: ProjectsService,
+              private vcwService: VcwService) { }
 
   ngOnInit(): void {
+    this.contextService.setProjectContext(undefined);
+    this.contextService.setVCWContext(undefined);
     this.route.events.pipe(takeUntil(this.componentDestroyed$)).subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.currentRoute = event.urlAfterRedirects;
+        this.getCurrentProjectAndVCW();
       }
     });
 
@@ -65,6 +71,33 @@ export class MainLayoutComponent implements OnInit, OnDestroy {
 
   onLogout() {
     this.keycloak.logout();
+  }
+
+  get selectedProject() {
+    return this.contextService.getProjectContext()?.name ?? '';
+  }
+
+  get selectedVCW() {
+    return this.contextService.getVCWContext()?.name ?? '';
+  }
+
+  getCurrentProjectAndVCW() {
+    const projectPath = this.currentRoute.split('/projects/');
+    const vcwPath = this.currentRoute.split('/vcw/');
+    if (projectPath.length > 1) {
+      const projecID = projectPath[1][0];
+      this.projectService.getProjectById(parseFloat(projecID)).pipe(take(1))
+      .subscribe((projectData) => {
+        this.contextService.setProjectContext(projectData);
+        if (vcwPath.length > 1) {
+          const vcwID = vcwPath[1][0];
+          this.vcwService.getVcw(parseFloat(projecID), parseFloat(vcwID)).pipe(take(1))
+          .subscribe((vcwData) => {
+            this.contextService.setVCWContext(vcwData);
+          })
+        }
+      })
+    }
   }
 
 }
